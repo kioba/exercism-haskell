c45ea78aeb44f0a916223f6f370c3ee88257798f
kioba <charlssom@gmail.com>
2020-07-28 00:17:26 +0100

    :sparkles: Bob solution

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,4 +1,40 @@
-module Bob (responseFor) where
+{-# LANGUAGE OverloadedStrings #-}
+module Bob
+  ( responseFor
+  )
+where
+
+import           Data.Char
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | isYelling xs && hasQuestionMark xs = "Calm down, I know what I'm doing!"
+  | hasQuestionMark xs                 = "Sure."
+  | isYelling xs                       = "Whoa, chill out!"
+  | isAllSpace xs                      = "Fine. Be that way!"
+  | otherwise                          = "Whatever."
+
+isYelling :: String -> Bool
+isYelling [] = False
+isYelling xs = allUpper $ filter isLetter xs
+ where
+  allUpper [] = False
+  allUpper x  = all isUpper x
+
+hasQuestionMark :: [Char] -> Bool
+hasQuestionMark [] = False
+hasQuestionMark xs = 
+  let a = trim xs
+  in '?' == (last $ orSpace a)
+
+orSpace :: String -> String
+orSpace [] = " "
+orSpace xs = xs
+
+isAllSpace :: String -> Bool
+isAllSpace []       = True
+isAllSpace (x : xs) = isSpace x && isAllSpace xs
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
